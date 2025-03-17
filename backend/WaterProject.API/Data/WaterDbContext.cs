b57e9521c5645c4a7ed0ea3698fdba52693c047e
AutPort <ajs289@byu.edu>
2025-03-17 14:50:53 -0600

    Initial commit

---
+++ backend/WaterProject.API/Data/WaterDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WaterProject.API.Data
+{
+    public class WaterDbContext : DbContext
+    {
+        public WaterDbContext(DbContextOptions<WaterDbContext> dbContext) : base(dbContext) 
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
