d26d38f9da256aec6a615ccc368cf92ec847f5f9
AutPort <ajs289@byu.edu>
2025-03-28 16:15:04 -0600

    git ignore backend update

--- backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
+++ backend/WaterProject.API/obj/Debug/net8.0/WaterProject.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c87cb6cf2eff9337b6b55f85dd66a986aa1e2364")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9699763cf85781333c1bafadaca92a90b16d5a98")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
